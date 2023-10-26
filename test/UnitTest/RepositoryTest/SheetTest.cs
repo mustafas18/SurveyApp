fe55e132caed812a9dbc2a0c3bcacca7c8f69498
Mostafa <mostafabazghandi2001@gmail.com>
2023-10-26 14:11:59 +0330

    UnitTest: Repository

--- test/UnitTest/RepositoryTest/SheetTest.cs
+++ test/UnitTest/RepositoryTest/SheetTest.cs
@@ -27,5 +27,42 @@ namespace UnitTest.RepositoryTest
             // Assert
 
         }
+
+        [Fact]
+        public void AddSheet_Should_Call_DbContext()
+        {
+            // Arrange
+            var dbContext = new Mock<AppDbContext>();
+            var dbSetCourseMock = new Mock<DbSet<Course>>();
+            dbContext.Setup(x => x.Set<Course>()).Returns(dbSetCourseMock.Object);
+            var course = new Course { Id = 1, TitleEn = "Course1" };
+            var course2 = new Course { Id = 2, TitleEn = "Course2" };
+
+            // Act
+            var repository = new Repository<Course>(dbContext.Object);
+            repository.Add(course);
+
+            //Assert
+            dbContext.Verify(x => x.Set<Course>());
+        }
+        [Fact]
+        public void AddSheet_Should_Add_Sheet1_To_dbSet()
+        {
+
+            // Arrange
+            var dbContext = new Mock<AppDbContext>();
+            var dbSetUserMock = new Mock<DbSet<Course>>();
+            dbContext.Setup(x => x.Set<Course>()).Returns(dbSetUserMock.Object);
+            var course = new Course { Id = 1, TitleEn = "Course1" };
+            var course2 = new Course { Id = 2, TitleEn = "Course2" };
+
+            // Act
+            var repository = new Repository<Course>(dbContext.Object);
+            repository.Add(course);
+
+            //Assert
+            dbSetUserMock.Verify(x => x.Add(It.Is<Course>(y => y == course)));
+        }
+
     }
 }
